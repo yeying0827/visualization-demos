e5d1461ab1e4a5d04a699644fd5b3f7049ca3df3
ying.ye <yeying0827@126.com>
2024-03-28 12:38:41 +0800

    fix: load image problem

--- src/pages/Filter/CSS.vue
+++ src/pages/Filter/CSS.vue
@@ -1,7 +1,12 @@
 <template>
   <h3>CSS滤镜</h3>
   <h4>灰度化</h4>
-  <img src="src/assets/girl1.jpg" alt="" style="filter: grayscale(100%);">
+  <img :src="girl1" alt="" style="filter: grayscale(100%);">
   <h4>叠加使用</h4>
-  <img src="src/assets/girl2.jpg" alt="" style="filter: blur(1.2px) grayscale(0.5) saturate(1.2) contrast(1.1) brightness(1.2);">
+  <img :src="girl2" alt="" style="filter: blur(1.2px) grayscale(0.5) saturate(1.2) contrast(1.1) brightness(1.2);">
 </template>
+
+<script setup>
+import girl1 from '/src/assets/girl1.jpg';
+import girl2 from '/src/assets/girl2.jpg';
+</script>
