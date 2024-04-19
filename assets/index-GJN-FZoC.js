5819ebd6eba8a5920376cbef0f097b7133f7b73d
yeying0827 <yeying0827@users.noreply.github.com>
2024-04-19 07:23:09 +0000

    deploy: cf01b16f423d472f96bab1d346c357fda74a52af

---
+++ assets/index-GJN-FZoC.js
@@ -0,0 +1,1 @@
+import{M as o,H as n}from"./HorizontalTab-O24cxzAI.js";import{r,o as s,c as l,e,F as m}from"./index-2p43lkDg.js";const b={__name:"index",setup(c){const t=[{path:"/webgl-basis/triangle",name:"三角形"},{path:"/webgl-basis/polygon",name:"多边形"}];return(i,p)=>{const a=r("router-view");return s(),l(m,null,[e(o,{title:"WebGL入门基础"}),e(n,{menus:t}),e(a)],64)}}};export{b as default};
